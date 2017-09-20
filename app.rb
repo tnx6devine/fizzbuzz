66ad1b0cc681b3d95539b3ee1e760fa64affd668
Trixie Devine <Devine@TC8-MACTEACHER.local>
2017-09-20 14:45:42 -0400

    finished fizzbuzz project

---
+++ app.rb
@@ -0,0 +1,18 @@
+# numbers = []
+counter = 0
+
+100.times do
+	# numbers.push(counter)
+	counter = counter +1
+	if counter % 15 == 0
+		puts "fizzbuzz"
+	elsif counter % 3 == 0
+		puts "fizz"
+	elsif counter % 5 == 0
+		puts "buzz"
+	else
+		puts counter
+	end
+end
+
+# puts numbers
